ac9c1d3b016bf44a9aa58a98b451f024876c48a9
Scionoftruth <charlieallen132@hotmail.com>
2021-09-14 16:25:25 -0500

    Initial Code Commit

---
+++ SSUser/src/main/java/com/pp/app/SsUserApplication.java
@@ -0,0 +1,13 @@
+package com.pp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsUserApplication.class, args);
+	}
+
+}
